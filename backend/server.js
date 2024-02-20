739d4d42681c64949b27306801ec705dd2248dfc
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-02-20 17:30:31 +0530

    init

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require('cors'); 
+const app = express();
+require('dotenv').config();
+const dbconfig = require("./config/dbconfig");
+app.use(express.json());
+//const userRoute = require("./routes/userRoute"); 
+
+app.use(cors()); 
+
+//app.use('/api/user', userRoute);
+
+const port = process.env.PORT || 9000;
+
+console.log(process.env.MONGO_URL)
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port},`);
+});
+
+
