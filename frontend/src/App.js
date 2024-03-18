47d68d138c1e62172ade6e31b6237dde294fcc5f
rashi21 <rashini059@gmail.com>
2024-03-18 10:09:04 +0530

    rashini3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Layout } from 'antd';
+import StaffRegistrationForm from "./pages/StaffRegistrationForm";
+import StaffLeaveForm from './pages/StaffLeaveForm';
+import StaffSalaryForm from './pages/StaffSalaryForm';
 
 //import PublicRoute from "./components/publicroute";
 //import { Toaster } from "react-hot-toast";
@@ -14,14 +17,18 @@ function App() {
   return (
     <div>
     <BrowserRouter>
-    <Layout />
+    <Layout>
       <Routes>
         <Route path="/" element={<Home/>} ></Route>    
+        <Route path="/register" element={<StaffRegistrationForm />} />
+        <Route path="/StaffLeave" element={<StaffLeaveForm />} />
+        <Route path="/StaffSalary" element={<StaffSalaryForm />} />
        </Routes>
-        
+      </Layout> 
     </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
