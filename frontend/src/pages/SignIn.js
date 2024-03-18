30e70fa5b7564170b2c2cf52873c6386eeee7874
OshadhaDaha <151594041+OshadhaDaha@users.noreply.github.com>
2024-03-18 18:00:24 +0530

    Oshi1

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Layout from '../components/Layout';
+
 import '../css/signin.css';
 
 function SignIn() {
   return (
+<<<<<<< HEAD
+    <Layout>
+    <body>
+=======
     <div className="container" style={{ marginTop: '100px', width: '600px', maxWidth: '400px', padding: '20px', border: '1px solid #ccc', borderRadius: '5px', backgroundColor: 'rgba(46, 119, 107, 0.397)'}}>
 
       <h2><center>Sign In To PetZone</center></h2>
+>>>>>>> 667c6dcd5c925e822d0f9c798ea0a7f5c224fb34
       <br></br>
+    <div className="signinform">
+      <h2><center>Sign In To PetZone</center></h2>
       <form>
         <div className="form-group">
           <label htmlFor="email">Email</label>
@@ -27,6 +36,9 @@ function SignIn() {
         </div>
       </form>
     </div>
+    <br></br>
+    </body>
+    </Layout>
   );
 }
 
