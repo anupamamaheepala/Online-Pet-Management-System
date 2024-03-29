bdf3409fe6b11e9c7052ad92903dcaf36cafc09b
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-03-29 15:37:22 +0530

    anu23

--- frontend/src/pages/Payerinfo.js
+++ frontend/src/pages/Payerinfo.js
@@ -57,7 +57,7 @@ const Payerinfo = () => {
                         onChange={(e) => setAddress(e.target.value)}
                     />
                 </div>
-                <center><button className='anupfbutton' type="submit">Submit</button></center>
+                <center><button className="anupfbutton" type="View Status">View Status</button></center>
             </form>
         </div>
         <Footer />
