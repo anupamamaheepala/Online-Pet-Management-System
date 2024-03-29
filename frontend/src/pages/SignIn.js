52bbda6c097fdc76f2cfec76440be46b2eda74c7
Subo <subodhinisanka@gmail.com>
2024-03-29 13:34:32 +0530

    suuu5

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Layout from '../components/Layout';
-
+import Layout from '../components/Header';
+import Footer from '../components/Footer';
 import '../css/signin.css';
 
 function SignIn() {
