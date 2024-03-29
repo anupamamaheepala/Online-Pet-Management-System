3cb7b269e0c4a0e03dcdb097015fde6ff6b8bfc1
Malinda Umesh <malindaumesh093@gmail.com>
2024-03-29 14:19:23 +0530

    malinda new 3

--- frontend/src/pages/MyAdvertisements.js
+++ frontend/src/pages/MyAdvertisements.js
@@ -9,23 +9,23 @@ const MyAdvertisements = () => {
         <>
             <Header /> 
            
-            <div className="advertisement-container">
-                <div className="advertisement-column">
+            <div className="ma_advertisement-container">
+                <div className="ma_advertisement-column">
                     <h3>Pets for sale</h3>
-                    <div className="advertisement-box">
-                        <img src="pet1.jpg" alt="Pet for sale" className="advertisement-photo" />
-                        <div className="advertisement-details">
+                    <div className="ma_advertisement-box">
+                        <img src="pet1.jpg" alt="Pet for sale" className="ma_advertisement-photo" />
+                        <div className="ma_advertisement-details">
                             <h4>Ad Title</h4>
                             <p>Description of the pet for sale.</p>
                             <p>Price: LKRXXX</p>
                             <p>Contact details</p>
-                            <div className="advertisement-buttons">
-                         <div className="button-container">
-        <Link to="/AddAdvertisement" className="add_button confirm_button">Edit</Link>
+                            <div className="ma_advertisement-buttons">
+                         <div className="ma_button-container">
+        <Link to="/AddAdvertisement" className="ma_add_button ma_confirm_button">Edit</Link>
             
         </div>
-        <div className="button-container">
-            <Link to="/MyAdvertisements" className="add_button reject_button">Delete</Link>
+        <div className="ma_button-container">
+            <Link to="/MyAdvertisements" className="ma_add_button ma_reject_button">Delete</Link>
         </div>
     </div>
                         </div>
