81426a8f434df101b000600fef1dc70b1113ecaf
Subo <subodhinisanka@gmail.com>
2024-03-29 12:27:25 +0530

    suuu4

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-//const User = require("../models/userModel");
+const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 //const authMiddleware = require("../middlewares/authMiddleware");
