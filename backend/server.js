b24de8ddbd0974881abedb7cae8227855b013260
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-04-01 20:19:10 +0530

    anu29

--- backend/server.js
+++ backend/server.js
@@ -6,35 +6,20 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const dbconfig = require("./config/dbconfig");
 app.use(express.json());
-<<<<<<< HEAD
-=======
 
 //const registerRoute = require('./routes/registerRoute');
-
-<<<<<<< HEAD
-
-=======
->>>>>>> a4cff5767993e286b1aeeffaf769fb4fe72e1aa8
 //const userRoute = require("./routes/userRoute");
->>>>>>> 98bda11ae68754af4cf2ea1a7ca7b4ee0bfc65f0
 
 
 const adsRoute = require("./routes/adverisementRoute");
 const customerRoute = require("./routes/registerRoute");
-
-
 const payerinfoRoute = require("./routes/paymentRoute");
 
 app.use(cors());
 app.use(bodyParser.json());
-<<<<<<< HEAD
-=======
-
->>>>>>> 98bda11ae68754af4cf2ea1a7ca7b4ee0bfc65f0
 
 app.use("/ads", adsRoute);
 app.use("/customer", customerRoute);
-
 app.use("/payerinfo", payerinfoRoute);
 
 app.get("/", (req, res) => {
