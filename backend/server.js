d97c6f80f400640578c1503744b9332e03d93291
Sathyajith <Sathyajith@gmail.com>
2024-04-04 20:30:14 +0530

    server fixed 2

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ require("dotenv").config();
 const dbconfig = require("./config/dbconfig");
 app.use(express.json());
 
-
 //const registerRoute = require('./routes/registerRoute');
 //const userRoute = require("./routes/userRoute");
 
@@ -28,13 +27,6 @@ const feedbacks = require('./routes/feedbackroute');
 //const cardpayRoute = require("./routes/cardpayRoute");
 //const makeAppointmentRoute = require ("./routes/MakeAppointmentRoute");
 
-//middleware
-// app.use((req, res, next) => {
-//   console.log(req.path, req.method);
-//   next();
-// });
-
-
 app.use(cors());
 app.use(bodyParser.json());
 //app.use('/api/register', registerRoute);
@@ -49,9 +41,6 @@ app.use("/product", productRoute);
 app.use("/banktrans", banktransRoute);
 app.use("/feedback", feedbacks);
 app.use("/appointment", appointmentRoutes);
-
-
-
 
 app.get("/", (req, res) => {
   res.json({ mssg: "Welcome to the app" });
