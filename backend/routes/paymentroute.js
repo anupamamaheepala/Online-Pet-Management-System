e970421cf68dd4d1a33aad51218009583de34bf1
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-04-06 22:31:34 +0530

    anu6-3

--- backend/routes/paymentroute.js
+++ backend/routes/paymentroute.js
@@ -4,11 +4,10 @@ const paymentController = require("../controller/PaymentController");
 
 // Route for adding payer information
 router.post("/pay", paymentController.addPayerInfo);
-//router.get("/:id", paymentController.getPayerInfoById);
 
+//router.get("/:id", paymentController.getPayerInfoById);
 // Route for fetching payer information by ID
 router.get("/:id", paymentController.getCustomerById);
-
 
 
 module.exports = router;
