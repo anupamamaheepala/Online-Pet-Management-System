b177175177ac076e1480c247e4c8d9497d297dcc
Subo <subodhinisanka@gmail.com>
2024-04-06 12:52:06 +0530

    suuuuu51

--- backend/routes/registerRoute.js
+++ backend/routes/registerRoute.js
@@ -36,6 +36,32 @@ router.put("/:id", registerController.updateCustomer);
 // Delete a customer by ID
 router.delete("/:id", registerController.deleteCustomerById);
 
+// // Sign-in endpoint
+// router.post('/signin', async (req, res) => {
+//   const { email, password } = req.body;
+
+//   try {
+//     // Check if the user exists in the database
+//     const user = await customerSchema.findOne({ email });
+
+//     if (!user) {
+//       return res.status(400).json({ message: 'Invalid credentials' });
+//     }
+
+//     // Compare the provided password with the hashed password in the database
+//     const isPasswordValid = await bcrypt.compare(password, Customer.password);
+
+//     if (!isPasswordValid) {
+//       return res.status(400).json({ message: 'Invalid credentials' });
+//     }
+
+//     // If the credentials are valid, return a success message
+//     res.status(200).json({ message: 'Sign-in successful', user });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Server Error' });
+//   }
+// });
 // Sign-in endpoint
 router.post('/signin', async (req, res) => {
   const { email, password } = req.body;
@@ -49,7 +75,7 @@ router.post('/signin', async (req, res) => {
     }
 
     // Compare the provided password with the hashed password in the database
-    const isPasswordValid = await bcrypt.compare(password, Customer.password);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
       return res.status(400).json({ message: 'Invalid credentials' });
