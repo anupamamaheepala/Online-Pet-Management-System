ce4aadcc4091124831e77098c764aa76062355c0
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-04-06 16:42:47 +0530

    anu6-2

--- backend/controller/PaymentController.js
+++ backend/controller/PaymentController.js
@@ -1,4 +1,37 @@
-// controllers/paymentController.js
+// // controllers/paymentController.js
+
+// const Payerinfo = require("../models/paymentModel");
+
+// // Controller function for handling payment-related operations
+// const paymentController = {
+//   // Method for adding payer information
+//   addPayerInfo: async (req, res) => {
+//     try {
+//       const { name, email, phonenumber, address } = req.body;
+
+//       if (!name || !email || !phonenumber || !address) {
+//         return res.status(400).json({ message: "All fields are required!" });
+//       }
+
+//       const income = new Payerinfo({
+//         name,
+//         email,
+//         phonenumber,
+//         address,
+//       });
+
+//       await income.save();
+//       res.status(200).json({ message: "Payer information added successfully" });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ message: "Server Error" });
+//     }
+//   },
+
+// };
+
+// module.exports = paymentController;
+
 
 const Payerinfo = require("../models/paymentModel");
 
@@ -13,24 +46,33 @@ const paymentController = {
         return res.status(400).json({ message: "All fields are required!" });
       }
 
-      const income = new Payerinfo({
+      const payerInfo = new Payerinfo({
         name,
         email,
         phonenumber,
         address,
       });
 
-      await income.save();
-      res.status(200).json({ message: "Payer information added successfully" });
+      await payerInfo.save();
+      res.status(200).json({ message: "Payer information added successfully", _id: payerInfo._id });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Server Error" });
     }
   },
+
+  // Method for getting payer information by ID
+  getCustomerById: async (req, res) => {
+    try {
+      const payerInfo = await Payerinfo.findById(req.params.id);
+      res.json(payerInfo);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to fetch payer details" });
+    }
+  },
 };
 
 module.exports = paymentController;
-
-
 
 
