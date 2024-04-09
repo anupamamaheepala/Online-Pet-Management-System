2f1b7e166833fe5688b15abea7661bae2f24af1d
Malinda Umesh <malindaumesh093@gmail.com>
2024-04-09 23:40:35 +0530

    malinda 4-9 last night

--- backend/models/advertisementModel.js
+++ backend/models/advertisementModel.js
@@ -1,3 +1,4 @@
+//advertisementModel.js
 
 const mongoose = require('mongoose');
 
@@ -49,55 +50,3 @@ const AddsSchema = new mongoose.Schema({
 module.exports = mongoose.model('Ads', AddsSchema)
 
 
-
-
-
-
-
-// // // advertisementModel.js
-
-// // const mongoose = require('mongoose');
-
-// // const advertisementSchema = new mongoose.Schema({
-// //   ownerName: {
-// //     type: String,
-// //     required: true
-// //   },
-// //   email: {
-// //     type: String,
-// //     required: true
-// //   },
-// //   title: {
-// //     type: String,
-// //     required: true
-// //   },
-// //   Breed: {
-// //     type: String,
-// //     required: true
-// //   },
-// //   purpose: {
-// //     type: String,
-// //     required: true
-// //   },
-// //   description: {
-// //     type: String,
-// //     required: true
-// //   },
-// //   price: {
-// //     type: String,
-// //     required: true
-// //   },
-// //   contact: {
-// //     type: String,
-// //     required: true
-// //   },
-// //   createdAt: {
-// //     type: Date,
-// //     default: Date.now
-// //   }
-// // });
-
-
-// // const Advertisement = mongoose.model('Advertisement', advertisementSchema);
-
-// // module.exports = Advertisement;
--- frontend/src/pages/ConfirmAdvertisement.js
+++ frontend/src/pages/ConfirmAdvertisement.js
@@ -1,3 +1,5 @@
+//ConfirmAdvertisement.js
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../css/advertisement.css';
