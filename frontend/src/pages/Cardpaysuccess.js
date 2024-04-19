5671329dfa9cc38cb2838b66768e98f59bd75df3
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-04-19 21:06:22 +0530

    anu19-4

--- frontend/src/pages/Cardpaysuccess.js
+++ frontend/src/pages/Cardpaysuccess.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../css/cardpaysuccess.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Cardpaysuccess = () => {
   const [payerDetails, setPayerDetails] = useState(null);
   const [cardDetails, setCardDetails] = useState(null);
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
@@ -29,6 +33,14 @@ const Cardpaysuccess = () => {
 
     fetchPayerDetails();
     fetchCardDetails();
+
+    // Update currentDateTime every second
+    const intervalId = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
   }, []);
 
   if (!payerDetails || !cardDetails) {
@@ -36,19 +48,30 @@ const Cardpaysuccess = () => {
   }
 
   return (
-    <div>
-      <h2>Payment Successful</h2>
-      <p>Congratulations! Your payment was successful.</p>
-      <h3>Payer Details:</h3>
-      <p>Name: {payerDetails.name}</p>
-      <p>Email: {payerDetails.email}</p>
-      <p>Phone Number: {payerDetails.phonenumber}</p>
-      <p>Address: {payerDetails.address}</p>
-      <p>Purpose: {payerDetails.purpose}</p>
-      <p>Amount: {payerDetails.amount}</p>
-      <p>Name on Card: {cardDetails.nameOnCard}</p>
-      <p>Card Number: {cardDetails.cardNumber}</p>
-    </div>
+    <>
+      <Header />
+      <div className="cps-container">
+        <h2>Payment Successful
+          <svg xmlns="http://www.w3.org/2000/svg" className="check-mark" viewBox="0 0 24 24" fill="green" width="48" height="48">
+          <path d="M0 0h24v24H0V0z" fill="none"/>
+          <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/>
+          </svg>
+        </h2>
+        <h5>Congratulations! Your payment was successful.</h5>
+        <h3>Payment Acknowledgement</h3>
+        <p><b>Name:</b> {payerDetails.name}</p>
+        <p><b>Email:</b> {payerDetails.email}</p>
+        <p><b>Phone Number:</b> {payerDetails.phonenumber}</p>
+        <p><b>Address:</b> {payerDetails.address}</p>
+        <p><b>Purpose:</b> {payerDetails.purpose}</p>
+        <p><b>Amount:</b> {payerDetails.amount}</p>
+        <p><b>Name on Card:</b> {cardDetails.nameOnCard}</p>
+        <p><b>Card Number:</b> {cardDetails.cardNumber}</p>
+        <p><b>Date:</b> {currentDateTime.toLocaleDateString()}</p>
+        <p><b>Time:</b> {currentDateTime.toLocaleTimeString()}</p>
+      </div>
+      <Footer />
+    </>
   );
 };
 
