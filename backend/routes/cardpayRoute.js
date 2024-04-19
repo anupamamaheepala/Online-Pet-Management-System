ba1ec6bac6b3ccbdab8c4c3bd0198ebd6ebd1ac1
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-04-19 10:31:07 +0530

    anu19-1

--- backend/routes/cardpayRoute.js
+++ backend/routes/cardpayRoute.js
@@ -1,3 +1,19 @@
+// const express = require("express");
+// const router = express.Router();
+// const cardpayController = require("../controller/cardpayController");
+
+// // Route for processing card payment
+// router.post("/cpay", cardpayController.processCardPayment);
+
+// // Route for getting all card payments with payer info
+// router.get("/cardpayments", cardpayController.getAllCardPaymentsWithPayerInfo);
+
+// // Route for fetching card payment report
+// //router.get("/cardpayreport", cardpayController.getCardpayReport);
+// router.get('/cardpayments/:id', cardpayController.getCardPaymentById);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const cardpayController = require("../controller/cardpayController");
@@ -8,7 +24,7 @@ router.post("/cpay", cardpayController.processCardPayment);
 // Route for getting all card payments with payer info
 router.get("/cardpayments", cardpayController.getAllCardPaymentsWithPayerInfo);
 
-// Route for fetching card payment report
-router.get("/cardpayreport", cardpayController.getCardpayReport);
+// Route for getting card payment by ID
+router.get("/cardpayments/:id", cardpayController.getCardPaymentById);
 
 module.exports = router;
