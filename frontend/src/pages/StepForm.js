455349a5006d88273ddaea50030477e049e4bbb9
alokaar <subathiaswini2002@gmail.com>
2024-04-21 10:15:27 +0530

    Aloka38th

--- frontend/src/pages/StepForm.js
+++ frontend/src/pages/StepForm.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'; // Import Link component
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../css/advertisement.css';
+import '../css/Trainingprogram.css';
 
 const StepForm = () => {
+    const [privatetrainings, setPrivatetrainings] = useState([]);
+    const [error, setError] = useState(null);
+    const [expandedCardIndex, setExpandedCardIndex] = useState(null);
     const [formData, setFormData] = useState({
         step: '',
         name: '',
@@ -13,6 +18,21 @@ const StepForm = () => {
         file: null,
         contact: ''
     });
+    useEffect(() => {
+        // Fetch data
+        axios.get('http://localhost:9000/step/getss')
+            .then(response => {
+                setPrivatetrainings(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching steps:', error);
+                setError(error);
+            });
+    }, []);
+    const handleLearnMoreClick = (index) => {
+        setExpandedCardIndex(index);
+    };
+
 
     const { step,name,title,description,file,contact } = formData;
 
@@ -101,6 +121,37 @@ const StepForm = () => {
 
                 <button style={{ width: '150px' }} type="submit" className="ma_submit-button">Add step</button>
             </form>
+            <h2 className="training-topic">Private Training Programs</h2>
+            <div className="col text-right">
+            <div className="col">
+                <Link to='/privateTraining'>
+                    <button className="trainingadd-apply-button">Apply</button>
+                </Link>
+                </div>
+            </div>
+            <div className="privatetraining-grid">
+                {privatetrainings.map((privatetraining, index) => (
+                    <div key={privatetraining._id} className="privatetraining-item">
+                        <img src={`http://localhost:9000/${privatetraining.filePath.replace(/\\/g, '/')}`}
+                            style={{ width: '230px', height: '200px' }} className="privatetraining-image" />
+
+                        <h4 className="privatetraining-step">{privatetraining.step}</h4>
+                        <div className="privatetraining-details">
+                            <h3 className="privatetraining-name">{privatetraining.name}</h3>
+                            {expandedCardIndex === index && (
+                                <p className="privatetraining-description">{privatetraining.description}</p>
+                            )}
+                            <div className="row justify-content-center">
+                                <div className="col-auto">&nbsp;</div>
+                                <div className="col">
+                                    <button className="add-button" onClick={() => handleLearnMoreClick(index)}>Learn more</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            
             <Footer />
         </>
     );
